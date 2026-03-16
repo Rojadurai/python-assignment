b4175d080480d84acf531ba7320a2e6a73c2244f
Roja <23ifte038@ldc.edu.in>
2026-03-16 16:36:39 +0530

    Apache Logg parser assignment

---
+++ hello.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+res1 = a + b
+res2 = a - b
+res3 = a * b
+res4 = a / b
+res5 = a % b
+print("Addition       : ",res1)
+print("Subtraction    : ",res2)
+print("Multiplication : ",res3)
+print("Division       : ",res4)
+print("Modulo         : ",res5)
